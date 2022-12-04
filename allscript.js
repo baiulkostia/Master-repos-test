0caccdf3f5ab8d6cd841e3fb1ace42763b106a94
Baiul Kostia <kostya.kadmin@gmail.com>
2022-12-04 13:18:42 +0200

    new code

---
+++ allscript.js
@@ -0,0 +1,7 @@
+const hoursNow = prompt('Скільки зараз годин?');
+
+const secondInMinuts = 60;
+const minutsInHour = 60;
+const secondInHour = secondInMinuts * minutsInHour;
+
+alert(hoursNow * secondInHour);
